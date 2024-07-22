14fd95beac3e69f1c6fc8675601f650f66db0749
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-07-22 18:21:35 +0530

    fetch users from firebase

--- app/(app)/home.js
+++ app/(app)/home.js
@@ -1,14 +1,50 @@
-import { View, Text, Button } from 'react-native'
-import React from 'react'
+import { View, StatusBar, ActivityIndicator } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
+import ChatList from '../../components/ChatList'
+import { useAuth } from '../../context/authContext'
+import { getDocs, query, where } from 'firebase/firestore'
+import { usersRef } from '../../firebaseConfig'
+// import Loading from '../../components/Loading'
 
 export default function Home() {
+  const { user } = useAuth()
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    if (user?.uid) {
+      getUsers()
+    }
+  }, [])
+
+  const getUsers = async () => {
+    const q = query(usersRef, where('userId', '!=', user?.uid))
+    const querySnapshot = await getDocs(q)
+    let data = [];
+    querySnapshot.forEach((doc) => {
+      data.push({...doc.data()})
+    })
+    setUsers(data)
+  }
 
   return (
     <View style={{
       flex: 1,
       backgroundColor: 'white'
     }}>
-      <Text>Home</Text>
+      <StatusBar style='light'/>
+      {users.length > 0 ? (
+        <ChatList users={users}/>
+      ):(
+        <View style={{
+          top: hp(30),
+          flex: 1,
+          alignItems: 'center'
+        }}>
+          <ActivityIndicator size='large' />
+          {/* <Loading size={hp(10)}/> */}
+        </View>
+      )}
     </View>
   )
 }
--- components/ChatItem.js
+++ components/ChatItem.js
@@ -1,6 +1,8 @@
-import { View, Text, TouchableOpacity, Image } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
+import { Image } from 'expo-image';
+import { blurhash } from '../utils/common';
 
 export default function ChatItem({ item, index, router, noBorder }) {
     const baseStyle = {
@@ -19,9 +21,15 @@ export default function ChatItem({ item, index, router, noBorder }) {
 
     return (
         <TouchableOpacity style={{ ...baseStyle, ...borderStyle }}>
-            <Image
-                source={require('../assets/images/avatar.png')}
+            {/* <Image
+                source={{uri: item?.profileUrl}}
                 style={{ height: hp(6), width: hp(6), borderRadius: 100 }}
+            /> */}
+            <Image
+                style={{ height: hp(6), width: hp(6), borderRadius: 100 }}
+                source={item?.profileUrl}
+                placeholder={blurhash}
+                transition={500}
             />
             <View style={{ flex: 1, marginLeft: 12 }}>
                 <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
@@ -30,7 +38,7 @@ export default function ChatItem({ item, index, router, noBorder }) {
                         fontWeight: '600',
                         color: '#1F2937'
                     }}>
-                        Lasindu
+                        {item?.username}
                     </Text>
                     <Text style={{
                         fontSize: hp(1.6),
