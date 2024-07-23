7468be26a8d03071bc8a4902d3fded86de7865dd
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-07-23 22:32:32 +0530

    create room and message collections

--- app/(app)/chatRoom.js
+++ app/(app)/chatRoom.js
@@ -1,5 +1,5 @@
-import { TextInput, TouchableOpacity, View } from 'react-native'
-import React, { useState } from 'react'
+import { Alert, TextInput, TouchableOpacity, View } from 'react-native'
+import React, { useEffect, useRef, useState } from 'react'
 import { useLocalSearchParams, useRouter } from 'expo-router'
 import { StatusBar } from 'expo-status-bar';
 import ChatRoomHeader from '../../components/ChatRoomHeader';
@@ -7,12 +7,53 @@ import MessagesList from '../../components/MessagesList';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
 import { Feather } from '@expo/vector-icons';
 import CustomeKeyboardView from '../../components/CustomeKeyboardView';
+import { useAuth } from '../../context/authContext'
+import { getRoomId } from '../../utils/common';
+import { addDoc, collection, doc, setDoc, Timestamp } from 'firebase/firestore';
+import { db } from '../../firebaseConfig';
 
 export default function ChatRoom() {
     const item = useLocalSearchParams()
     // console.log(item);
+    const { user } = useAuth()
     const router = useRouter()
     const [messages, setMessages] = useState([])
+    const textRef = useRef('')
+    const inputRef = useRef(null)
+
+    useEffect(() => {
+        createRoomIfNotExists()
+    }, [])
+
+    const createRoomIfNotExists = async () => {
+        let roomId = getRoomId(user?.userId, item?.userId)
+        await setDoc(doc(db, 'rooms', roomId), {
+            roomId,
+            createdAt: Timestamp.fromDate(new Date())
+        })
+    }
+
+    const handleSendMessage = async () => {
+        let message = textRef.current.trim()
+        if (!message) return
+        try {
+            let roomId = getRoomId(user?.userId, item?.userId)
+            const docRef = doc(db, 'rooms', roomId)
+            const messagesRef = collection(docRef, 'messages')
+            textRef.current = ''
+            if (inputRef) inputRef.current.clear()
+            const newDoc = await addDoc(messagesRef, {
+                userId: user?.userId,
+                text: message,
+                username: user?.username,
+                profileUrl: user?.profileUrl,
+                createdAt: Timestamp.fromDate(new Date())
+            })
+            console.log('new message id: ', newDoc.id)
+        } catch (error) {
+            Alert.alert('Message', error.message)
+        }
+    }
 
     return (
         <CustomeKeyboardView inchat={true}>
@@ -38,15 +79,20 @@ export default function ChatRoom() {
                             paddingLeft: 20
                         }}>
                             <TextInput
+                                ref={inputRef}
+                                onChangeText={(value) => textRef.current = value}
                                 placeholder='Type Message...'
                                 style={{ fontSize: hp(2), flex: 1, marginRight: 8 }}
                             />
-                            <TouchableOpacity style={{
-                                backgroundColor: '#E5E7EB',
-                                padding: 8,
-                                marginRight: 1,
-                                borderRadius: 100
-                            }}>
+                            <TouchableOpacity
+                                style={{
+                                    backgroundColor: '#E5E7EB',
+                                    padding: 8,
+                                    marginRight: 1,
+                                    borderRadius: 100,
+                                }}
+                                onPress={handleSendMessage}
+                            >
                                 <Feather name='send' size={hp(2.7)} color='#737373' />
                             </TouchableOpacity>
                         </View>
